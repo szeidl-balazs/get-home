cdbbf4cf34b913f29469a88d04cc9c4da09585b9
bszeidl <szeidl.balazs@hotmail.com>
2022-06-25 18:42:13 +0200

    Code refactored, variables withoin the function.

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var totalDistanceToTravel = prompt('Hány kilométer távolságot kíván megtenni? Adjon meg egy számot!');
 
-var averageLitersOverHundred = prompt('Mi az űrhajó átlagos fogyasztása literben? Adjon meg egy számot!');
-
-var tankCapacity = prompt('Mi az űrhajó üzemanyagtartályának űrtartalma literben? Adjon meg egy számot!');
 
 
 
 function calculateFuelToOffload() {
-   
-   var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100);
-   console.log('Szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L');
-   alert('Szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L');
 
+    var totalDistanceToTravel = prompt('Hány kilométer távolságot kíván megtenni? Adjon meg egy számot!');
+
+    var averageLitersOverHundred = prompt('Mi az űrhajó átlagos fogyasztása literben? Adjon meg egy számot!');
+
+    var tankCapacity = prompt('Mi az űrhajó üzemanyagtartályának űrtartalma literben? Adjon meg egy számot!');
    
+    var requiredFuel = totalDistanceToTravel * (averageLitersOverHundred / 100); 
+
     var fuelToOffload = tankCapacity - requiredFuel;
-    console.log('Leeresztendő üzemanyag: ' + fuelToOffload + ' L');
-    alert('Leeresztendő üzemanyag: ' + fuelToOffload + ' L');
+
+    alert('Az utazáshoz szükséges üzemanyag-mennyiség: ' + requiredFuel + ' L\n' + 'Leadandó üzemanyag-mennyiség: ' + fuelToOffload + ' L');
+    
 }
 
 
